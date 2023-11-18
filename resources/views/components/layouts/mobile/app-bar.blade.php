1c2b349b54a76f623c8ee528ac60b08f4ae8a6a8
Abdallah Samy <abdallhsamy2011@gmail.com>
2023-11-19 02:57:03 +0300

    move script to app.js

--- resources/views/components/layouts/mobile/app-bar.blade.php
+++ resources/views/components/layouts/mobile/app-bar.blade.php
@@ -45,18 +45,18 @@
     </a>
 </div>
 
-@push('scripts')
-    <script>
-        addEventListener('DOMContentLoaded', function() {
-            document.querySelectorAll('.app-bar-item').forEach(function (item) {
-                if(item.getAttribute('href') === window.location.href) {
-                    item.classList.add('text-primary');
-                    item.classList.remove('text-gray-500');
-                } else {
-                    item.classList.remove('text-primary');
-                    item.classList.add('text-gray-500');
-                }
-            })
-        })
-    </script>
-@endpush
+{{--@push('scripts')--}}
+{{--    <script>--}}
+{{--        addEventListener('DOMContentLoaded', function() {--}}
+{{--            document.querySelectorAll('.app-bar-item').forEach(function (item) {--}}
+{{--                if(item.getAttribute('href') === window.location.href) {--}}
+{{--                    item.classList.add('text-primary');--}}
+{{--                    item.classList.remove('text-gray-500');--}}
+{{--                } else {--}}
+{{--                    item.classList.remove('text-primary');--}}
+{{--                    item.classList.add('text-gray-500');--}}
+{{--                }--}}
+{{--            })--}}
+{{--        })--}}
+{{--    </script>--}}
+{{--@endpush--}}
