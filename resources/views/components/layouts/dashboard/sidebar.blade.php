d27565b54a0de34eaf9cbb12b18845a0ecf456a0
Abdallah Samy <abdallhsamy2011@gmail.com>
2023-11-19 16:57:43 +0300

    init dashboard sidebar

--- resources/views/components/layouts/dashboard/sidebar.blade.php
+++ resources/views/components/layouts/dashboard/sidebar.blade.php
@@ -2,15 +2,15 @@
     <x-logo color="white" class="ms-8 me-12 mt-4"/>
     <hr class="border-shade my-4 me-4">
     <ul class=" flex flex-col ">
-        <li class="flex gap-4 text-white p-2 ">
+        <li class=" ">
             <i data-feather="bar-chart"></i>
             dashboard
         </li>
-        <li class="active flex gap-4 font-bold text-primary bg-shade p-2 rounded-s-full transform transition-all duration-300 scale-100 hover:scale-105">
+        <li class="active">
             <i data-feather="align-center"></i>
             dashboard
         </li>
-        <li class="flex gap-4 text-white p-2">
+        <li class="">
             <i data-feather="anchor"></i>
             dashboard
         </li>
