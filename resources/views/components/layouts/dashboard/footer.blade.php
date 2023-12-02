983c255bd7bae1ed991d00a4459a76f4abc39cf4
Abdallah Samy <abdallhsamy2011@gmail.com>
2023-12-02 19:33:28 +0300

    fix footer style

--- resources/views/components/layouts/dashboard/footer.blade.php
+++ resources/views/components/layouts/dashboard/footer.blade.php
@@ -1,12 +1,12 @@
-<footer class="h-12 w-full absolute bottom-0 flex items-center justify-between p-4">
+<footer class="w-full absolute bottom-0 flex flex-col sm:flex-row items-center justify-between p-4">
     <div class="">
         <span>2023</span> &copy; <a href="{{ url('/') }}">{{ config('app.name') }}</a>
         - {{ __('general.all_rights_reserved') }}
     </div>
 
     <div class="">
-        {{ __('general.designed_and_developed_by') }} <a href="https://wa.me/201115992468"
-                                                         class="font-bold">{{ __('general.abdallah_samy') }}</a>
+        {{ __('general.designed_and_developed_by') }}
+        <a href="https://wa.me/201115992468" class="font-bold">{{ __('general.abdallah_samy') }}</a>
     </div>
 
 </footer>
