9181ea9641c5064493c33508a122fbe6a88aa46d
Abdallah Samy <abdallhsamy2011@gmail.com>
2023-12-02 19:12:26 +0300

    show  logged in user photo

--- resources/views/components/layouts/dashboard/topnav.blade.php
+++ resources/views/components/layouts/dashboard/topnav.blade.php
@@ -5,7 +5,7 @@
     <div class="flex items-center justify-between gap-4">
         <i data-feather="bell" class="text-xs text-[#475569]"></i>
 
-        <img src="https://randomuser.me/api/portraits/lego/5.jpg" alt="user avatar"
+        <img src="{{ auth()->user()->avatar }}" alt="user avatar"
              class="w-8 h-8 rounded-full overflow-hidden shadow-lg image-fit zoom-in">
     </div>
 </nav>
